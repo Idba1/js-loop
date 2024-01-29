50ffeabfa6a8033ae06b2b48c6e8404335e4fafd
Idba1 <moniraislamself@gmail.com>
2024-01-29 22:26:51 +0600

    for-of.js

--- for-of.js
+++ for-of.js
@@ -2,3 +2,11 @@ const numbers = [22, 44, 55, 11, 44, 55, 7, 88, 90];
 for (const number of numbers){
     console.log(number);
 }
+
+
+
+
+const fruits = ['mango', 'orange', 'banana', 'apple'];
+for(const fruit of fruits){
+    console.log(fruit);
+}
